d12364125e4521de77f79d0255e3b1a6930fec77
Stopher <chrismwangi022@gmail.com>
2026-05-23 12:02:56 +0300

    plotty fix

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ Main Streamlit Application
 
 import streamlit as st
 import pandas as pd
+import plotly.graph_objects as go
 import plotly.express as px
 from datetime import datetime, timedelta
 import sys
